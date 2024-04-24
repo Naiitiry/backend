292a367e417808289859a4e7145b5e84fad1c85f
Naiitiry <danchukroman@gmail.com>
2024-04-24 13:31:00 -0300

    statics y roters

--- main.py
+++ main.py
@@ -2,15 +2,17 @@
 
 from fastapi import FastAPI
 from routers import products, users
+from fastapi.staticfiles import StaticFiles
 
 # uvicorn main:app --reload --> inicia el servidor 
 
 app = FastAPI()
 
-# Routers
+# Routers, traido desde routers
 
 app.include_router(products.router)
 app.include_router(users.router)
+app.mount("/static",StaticFiles(directory="static"),name="static")
 
 # URL local: http://127.0.0.1:8000
 
